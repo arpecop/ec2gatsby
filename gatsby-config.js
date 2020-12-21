5228e1d9df30666c2ebfcac2f93582940d5868d1
Radoslav <rudix.lab@gmail.com>
2020-12-21 09:46:13 +0200

    c

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Bonneville`,
+    title: `userz`,
     titleTemplate: `%s | A starter theme for Gatsby`,
     description: `A starter theme for Gatsby`,
     siteUrl: `https://bonneville.netlify.com`,
