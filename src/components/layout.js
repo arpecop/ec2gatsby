b7c1b5fdfcbc8d56983eb520cc52136d309c56d2
Radoslav <rudix.lab@gmail.com>
2020-12-26 15:36:51 +0200

    c

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,59 @@ export default ({ children }) => (
           style={{ border: "none" }}
         ></iframe>
       </div>
-
+      <h1>Shop from Amazon Deals</h1>
+      <ul>
+        <li>
+          <a href="https://amzn.to/3mPKTYr" rel="nofollow">
+            Home & Kitchen
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/34LANS3" rel="nofollow">
+            Electronics
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/2M0qcfw" rel="nofollow">
+            Toys & Games
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/34HW8Ms" rel="nofollow">
+            Cell Phones & Accessories
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/38Chzzt" rel="nofollow">
+            Clothing, Shoes & Jewelry
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/3hkrtdd" rel="nofollow">
+            Baby
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/3rtELbI" rel="nofollow">
+            Video Games
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/3rxMHci" rel="nofollow">
+            Home & Business Services
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/3nUw5ZRR" rel="nofollow">
+            CDs & Vinyl
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/34KJFrn" rel="nofollow">
+            Books
+          </a>
+        </li>
+      </ul>
       {children}
       <div style={{ textAlign: "center" }}>
         <iframe
