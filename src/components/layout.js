23322d85f2cea9d1d3d638602abd728d7d440940
Radoslav <rudix.lab@gmail.com>
2020-12-26 15:00:10 +0200

    c

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,11 +18,23 @@ export default ({ children }) => (
           border="0"
           marginwidth="0"
           frameborder="0"
-          style={{ frameborder: 0 }}
+          style={{ border: "none" }}
         ></iframe>
       </div>
 
       {children}
+      <div style={{ textAlign: "center" }}>
+        <iframe
+          src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=49&l=ur1&category=topratedproducts&banner=1NKVVY70RBKZ8F0X0DR2&f=ifr&linkID=d5166c7712ff9037a0c38a0c327937d4&t=arpecop0c-20&tracking_id=arpecop0c-20"
+          width="300"
+          height="600"
+          scrolling="no"
+          border="0"
+          marginwidth="0"
+          frameborder="0"
+          style={{ border: "none" }}
+        ></iframe>
+      </div>
     </main>
     <Footer />
   </>
