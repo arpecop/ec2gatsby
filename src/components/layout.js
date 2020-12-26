ff622d3fa236b28c4f07d40a22c167ffda2d8c1e
Radoslav <rudix.lab@gmail.com>
2020-12-26 14:58:13 +0200

    c

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,22 @@ import "../scss/main.scss"
 export default ({ children }) => (
   <>
     <Header />
-    <main className="main-body">{children}</main>
+    <main className="main-body">
+      <div style={{ textAlign: "center" }}>
+        <iframe
+          src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=topgiftideas&banner=0CZRSHB5W9YA6H9M3502&f=ifr&linkID=c4fb05fd766ff70e88983b7b68683fbc&t=arpecop0c-20&tracking_id=arpecop0c-20"
+          width="300"
+          height="250"
+          scrolling="no"
+          border="0"
+          marginwidth="0"
+          frameborder="0"
+          style={{ frameborder: 0 }}
+        ></iframe>
+      </div>
+
+      {children}
+    </main>
     <Footer />
   </>
 )
